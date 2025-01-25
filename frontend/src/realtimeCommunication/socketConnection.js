59dbb5409e3cd64065aab0db61322156ed7dce05
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2025-01-26 00:15:04 +0600

    Update

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -5,6 +5,9 @@ import {
   setOnlineUsers,
 } from "../store/actions/friendsActions";
 import store from "../store/store";
+import { updateDirectChatHistoryIfActive } from "../shared/utils/chat";
+import * as roomHandler from "./roomHandler";
+import * as webRTCHandler from "./webRTCHandler";
 
 let socket = null;
 
@@ -18,7 +21,7 @@ export const connectWithSocketServer = (userDetails) => {
   });
 
   socket.on("connect", () => {
-    console.log("successfully connected with socket.io server");
+    console.log("succesfully connected with socket.io server");
     console.log(socket.id);
   });
 
@@ -36,4 +39,62 @@ export const connectWithSocketServer = (userDetails) => {
     const { onlineUsers } = data;
     store.dispatch(setOnlineUsers(onlineUsers));
   });
+
+  socket.on("direct-chat-history", (data) => {
+    console.log(data);
+    updateDirectChatHistoryIfActive(data);
+  });
+
+  socket.on("room-create", (data) => {
+    roomHandler.newRoomCreated(data);
+  });
+
+  socket.on("active-rooms", (data) => {
+    roomHandler.updateActiveRooms(data);
+  });
+
+  socket.on("conn-prepare", (data) => {
+    const { connUserSocketId } = data;
+    webRTCHandler.prepareNewPeerConnection(connUserSocketId, false);
+    socket.emit("conn-init", { connUserSocketId: connUserSocketId });
+  });
+
+  socket.on("conn-init", (data) => {
+    const { connUserSocketId } = data;
+    webRTCHandler.prepareNewPeerConnection(connUserSocketId, true);
+  });
+
+  socket.on("conn-signal", (data) => {
+    webRTCHandler.handleSignalingData(data);
+  });
+
+  socket.on("room-participant-left", (data) => {
+    console.log("user left room");
+    webRTCHandler.handleParticipantLeftRoom(data);
+  });
+};
+
+export const sendDirectMessage = (data) => {
+  console.log(data);
+  socket.emit("direct-message", data);
+};
+
+export const getDirectChatHistory = (data) => {
+  socket.emit("direct-chat-history", data);
+};
+
+export const createNewRoom = () => {
+  socket.emit("room-create");
+};
+
+export const joinRoom = (data) => {
+  socket.emit("room-join", data);
+};
+
+export const leaveRoom = (data) => {
+  socket.emit("room-leave", data);
+};
+
+export const signalPeerData = (data) => {
+  socket.emit("conn-signal", data);
 };
